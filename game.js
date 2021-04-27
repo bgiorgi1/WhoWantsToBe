d8bb95ad578539634a922a55e158aba4cae04cc3
“bgiorgi1” <brianna.giorgi@gmail.com>
2021-04-27 15:52:54 -0400

    updated

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById('progressText');
-const scoreText = document.getElementById('score');
-const progressBarFull = document.getElementById('progressBarFull');
+let question = document.getElementById('question'); //pulling in questions
+const choices = Array.from(document.getElementsByClassName('choice-text')); //pulls in array of question choices queryselectorall
+const progressText = document.getElementById('progressText');  //pulls in progress bar text "question 1/10"
+const scoreText = document.getElementById('score'); //pulls in display score
+const progressBarFull = document.getElementById('progressBarFull'); //pulls in progress bar
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -17,91 +17,14 @@ fetch(
         return res.json();
     })
     .then((loadedQuestions) => {
-        questions = loadedQuestions.results.map((loadedQuestion) => {
-            const formattedQuestion = {
-                question: loadedQuestion.question,
-            };
+    //    return questions = loadedQuestions.results.map(loadedQuestion => loadedQuestion);
+       for (let i=0; i < loadedQuestions.results.length; i++) {
+           console.log(i)
+           questions.push(loadedQuestions.results[i])
+       }     console.log(questions)
+       return questions
 
-            const answerChoices = [...loadedQuestion.incorrect_answers];
-            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-            answerChoices.splice(
-                formattedQuestion.answer - 1,
-                0,
-                loadedQuestion.correct_answer
-            );
-
-            answerChoices.forEach((choice, index) => {
-                formattedQuestion['choice' + (index + 1)] = choice;
-            });
-
-            return formattedQuestion;
-        });
-        startGame();
     })
-    .catch((err) => {
-        console.error(err);
-    });
+   console.log(questions)
 
-//CONSTANTS
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 10;
-
-startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuesions = [...questions];
-    getNewQuestion();
-};
-
-getNewQuestion = () => {
-    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score);
-        //go to the end page
-        return window.location.assign('/end.html');
-    }
-    questionCounter++;
-    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    //Update the progress bar
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-
-    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-    currentQuestion = availableQuesions[questionIndex];
-    question.innerText = currentQuestion.question;
-
-    choices.forEach((choice) => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
-
-    availableQuesions.splice(questionIndex, 1);
-    acceptingAnswers = true;
-};
-
-choices.forEach((choice) => {
-    choice.addEventListener('click', (e) => {
-        if (!acceptingAnswers) return;
-
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
-
-        const classToApply =
-            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-
-        if (classToApply === 'correct') {
-            incrementScore(CORRECT_BONUS);
-        }
-
-        selectedChoice.parentElement.classList.add(classToApply);
-
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 1000);
-    });
-});
-
-incrementScore = (num) => {
-    score += num;
-    scoreText.innerText = score;
-};
+   .catch(err=>console.log(err))
