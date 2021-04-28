014c5aded9147ef5c90c0bc3861f5db238d482ed
“bgiorgi1” <brianna.giorgi@gmail.com>
2021-04-28 11:29:12 -0400

    questions are displayed

--- game.js
+++ game.js
@@ -3,12 +3,43 @@ const choices = Array.from(document.getElementsByClassName('choice-text')); //pu
 const progressText = document.getElementById('progressText');  //pulls in progress bar text "question 1/10"
 const scoreText = document.getElementById('score'); //pulls in display score
 const progressBarFull = document.getElementById('progressBarFull'); //pulls in progress bar
-let currentQuestion = {};
+let currentQuestion = 0;
 let acceptingAnswers = false;
+let gameIndex = 0;
 let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 let questions = [];
+
+function shuffle(array) { //shuffle the questions/array
+    array.sort(() => Math.random() - 0.5);
+  }
+function showQuestions(index, mainQuestions) { //function takes in number and main questions
+console.log(mainQuestions)
+//takes in number of question you're on and counts to 10, so that their is a set number of questions you need to reach
+// relflects what number question you're on
+//if button is clicked and gets answer
+// then move progress bar
+currentQuestion = index
+let theQuestion=mainQuestions[index] //dictating where the questions are in the array, that is asks.
+question.innerHTML=theQuestion.question //pulls in the question to the site
+choices.map((answers, choiceIndex) => {
+    // answers.innerHTML=choiceIndex //inserted index number from choices
+    answers.innerHTML=`${mainQuestions[currentQuestion].possible_answers[choiceIndex]}` //pulls in answer choices to page
+}) 
+}
+function answerQuestion(){
+//turns answer red or green dpeending on whether you get it right or not.
+// if question is correct then turn button that was clicked green
+// else turn button red
+}
+
+function highScore (){
+    //once you answer the question, it shows up in the score, whether yoy get it right or wrong
+//10 points per question
+// if answer is correct, then add 10 points to score
+//else do not
+}
 
 fetch(
     'https://opentdb.com/api.php?amount=20&category=18&difficulty=easy&type=multiple'
@@ -19,12 +50,13 @@ fetch(
     .then((loadedQuestions) => {
     //    return questions = loadedQuestions.results.map(loadedQuestion => loadedQuestion);
        for (let i=0; i < loadedQuestions.results.length; i++) {
-           console.log(i)
-           questions.push(loadedQuestions.results[i])
-       }     console.log(questions)
-       return questions
+          loadedQuestions.results[i]["possible_answers"]=loadedQuestions.results[i]["incorrect_answers"] //this shows loadedquestions as possible answers
+          loadedQuestions.results[i]['possible_answers'].push(loadedQuestions.results[i]['correct_answer']); //pushing correct answer to array
+          shuffle(loadedQuestions.results[i]["possible_answers"])
+          questions.push(loadedQuestions.results[i]) 
+        }     
+       showQuestions(0, questions)
 
-    })
-   console.log(questions)
+    }).catch(err=>console.log(err)) //whatever error messages that happen, shows it
+//    console.log(questions)
 
-   .catch(err=>console.log(err))
--- reflections.md
+++ reflections.md
@@ -10,25 +10,25 @@
 - [x] What did you achieve yesterday?
 - [x] What are your goals for today?
 - [x] Do you have any blockers?
-1. RESPONSE
-2. RESPONSE
-3. RESPONSE
+1. watched a bunch of videos to try and make more sense of what we've been learning.  Researched a bunch on how I want my project 1 by watching some youtube videos that walked through the code.  Decided on a trivia based game and outlined goals for project. found an API i want to use to fetch the questions/answers
+2. Goals for today is to minimize frustration and discouragement by staying positive in the unknown. At the very least I would like to have some functonality in the game by EOD. 
+3. No blockers I can think of as yet, we'll see when we get going...
 
 # Tuesday
 - [x] What did you achieve yesterday?
 - [x] What are your goals for today?
 - [x] Do you have any blockers?
-1. RESPONSE
-2. RESPONSE
-3. RESPONSE
+1. Yesturday I found some really amazing tutorials that I applied to the skeleton of my project
+2. My goal today is to rewrite code so it makes sense to me
+3. My knowledge of functions and logic is my blocker.
 
 # Wednesday
 - [x] What did you achieve yesterday?
 - [x] What are your goals for today?
 - [x] Do you have any blockers?
-1. RESPONSE
-2. RESPONSE
-3. RESPONSE
+1. Yesturday we were able to get my API to fetch, so now we're working on getting the answers and questions to appear
+2. My goal today is to have a working trivia game
+3. My brain is my only blocker and still struggling with a lot of the application of what weve learned, but i also feel really supported by Paula and team so getting through it..
 
 # Thursday
 - [x] What did you achieve yesterday?
